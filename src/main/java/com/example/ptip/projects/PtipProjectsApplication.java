f2a940508798e90e49db83ea61492d3ad64c85c3
cji6463 <cji6463@ptu.ac.kr>
2025-06-26 16:21:51 +0900

    Initial commit of ptip Spring Boot project

---
+++ src/main/java/com/example/ptip/projects/PtipProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ptip.projects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtipProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtipProjectsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ptip/projects/PtipProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ptip.projects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtipProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
